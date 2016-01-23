1a2d27f3994b2307fb368ad30c5c02065f188683
ravkovskiy <kos1992@mail.ru>
2016-01-23 16:40:22 +0300

    Revert "а"
    This reverts commit 70f18a6943891cf8091a396f56997e7abae7a97a.

---
+++ js/controllers.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/* Controllers */
+
+var ElControllers = angular.module('ElControllers', []);
+
+ElControllers.controller('WebSiteCtrl', ['$scope', '$http',
+  function($scope, $http) {
+  }]);
+
+ElControllers.controller('PriceCtrl', ['$scope', '$routeParams',
+  function($scope, $routeParams) {
+  }]);
